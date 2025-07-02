0e8dc91fc80c36d8a56b8953205274ec1e134586
Erwin Llano <erwinllano95@gmail.com>
2025-07-02 08:55:39 +0800

    Answered some exercise

--- JsFundamentals/scripts/JsBasics.js
+++ JsFundamentals/scripts/JsBasics.js
@@ -1,3 +1,15 @@
 export function jsbasic() {
   alert("Hello");
 }
+
+const sum = 2 + 2;
+console.log(sum);
+
+document.body.innerHTML = "Hello";
+
+// Challenge Exercise
+const cost = 10 + 8 + 20;
+console.log("Total of t-shirt order is ", cost);
+
+const remainingMoney = 100 - 20 - 50 + 200;
+console.log("My money balance is ", remainingMoney);
