40f18769fe891fde9bb19bf7b7386a56adcf4080
Erwin Llano <erwinllano95@gmail.com>
2025-07-29 02:44:51 +0800

    Multi-line string

--- JsFundamentals/scripts/Strings.js
+++ JsFundamentals/scripts/Strings.js
@@ -22,8 +22,15 @@ export function string() {
   const num2 = "Item (" + (1 + 1) + "):  $" + (2095 + 799) / 100; // Concatenation
   console.log(num2);
 
+  // Template String
+  // Interpolation and Multi-line strings
+
   const num3 = `Item (${1 + 1}):  $${(2095 + 799) / 100}`; // Interpolation
   console.log(num3);
+
+  const num4 = `Some
+  text`; // Multi-line string
+  console.log(num4);
 
   // Interpolation - Insert a value durectly into a string
 
