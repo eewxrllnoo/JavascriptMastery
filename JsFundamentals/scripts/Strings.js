b133ca371a448476fa4faad8d8740d856a0e4ba3
Erwin Llano <erwinllano95@gmail.com>
2025-07-29 02:00:59 +0800

    Convert back to dollars

--- JsFundamentals/scripts/Strings.js
+++ JsFundamentals/scripts/Strings.js
@@ -1,4 +1,8 @@
 export function string() {
+  console.log(
+    "--------------------String Fundamentals---------------------------------"
+  );
+
   //alert("Hello, World!");
   const greeting = "Hello" + ", " + "World!";
   console.log(greeting);
@@ -6,4 +10,16 @@ export function string() {
   // Type coersion
   const numString = "Hello" + "3";
   console.log(numString);
+
+  // When calculating money
+  // 1.Calculate in cents
+  // 2.Convert back to dollars
+
+  // const num = "$" + (20.95 + 7.99);
+  const num = "$" + (2095 + 799) / 100;
+  console.log(num);
+
+  console.log(
+    "--------------------String Fundamentals---------------------------------"
+  );
 }
