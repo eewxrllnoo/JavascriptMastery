4e1848404c0f832233353ff408249d75f1330f6e
Erwin Llano <erwinllano95@gmail.com>
2025-07-29 00:00:58 +0800

    Update htmlcss component

--- JsFundamentals/scripts/htmlcss_js.js
+++ JsFundamentals/scripts/htmlcss_js.js
@@ -1,3 +1,7 @@
 export function htmlCss() {
   // ...
+  console.log("HTML and CSS Fundamentals");
+  // This function can be used to demonstrate HTML and CSS concepts
+  // such as elements, attributes, styles, and layout.
+  // You can add more functionality or examples as needed.
 }
